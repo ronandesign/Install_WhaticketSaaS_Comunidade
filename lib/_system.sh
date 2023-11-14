8bf403c4dc00cb3edf28ef70ba2722784f2416dd
Eduardo Policarpo <eduardo@Eduardos-iMac-Pro.local>
2023-11-14 14:13:42 -0300

    fix: alterado para NVM instalacao do nodejs

--- lib/_system.sh
+++ lib/_system.sh
@@ -285,10 +285,9 @@ system_node_install() {
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   nvm install --lts
   sleep 2
-  
-  sudo su - root <<EOF
   npm install -g npm@latest
-  sleep 2
+
+  sudo su - root <<EOF
   sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
   sudo apt-get update -y && sudo apt-get -y install postgresql
